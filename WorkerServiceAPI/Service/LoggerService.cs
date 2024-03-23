49e378286fc4aa8cd44465113bcc024d70a589aa
Franklin Cappa Ticona <franklincappa@gmail.com>
2024-03-23 17:35:31 -0500

    Creando servicio

--- WorkerServiceAPI/Service/LoggerService.cs
+++ WorkerServiceAPI/Service/LoggerService.cs
@@ -3,6 +3,11 @@
     public class LoggerService : ILoggerService
     {
         private readonly ILogger<LoggerService> _logger;
+
+        public LoggerService(ILogger<LoggerService> logger)
+        {
+            _logger = logger;
+        }
         public void WriteInLog(string cadena)
         {
             _logger.LogWarning(cadena);
